286b3cae87c25a1a2a7ad783f91daa0171ed75c1
Ama-10669107 <42800138+Ama-10669107@users.noreply.github.com>
2018-09-12 22:09:47 +0000

    Add files via upload

---
+++ a4_10669107.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main();
+int g(int a, int b);
+cout<<"enter int a and int b"<<"g"<<endl;
+cin>>"a,b";
+if (a>b)
+{
+	int c;
+	a/b == cxb + a%b
+	a%b=r
+	if (r==0)
+	{
+		cout<<"c is the g"<<endl;
+	}
+	else if (r>0)
+	{
+		int c;
+		b/r == c*r + b%r
+		if (b%r==0)
+		{
+			cout<<"c is the g"<<endl;
+		}
+		else
+		{
+			return a,b;
+		}
+	}
+	return 0;
+}
